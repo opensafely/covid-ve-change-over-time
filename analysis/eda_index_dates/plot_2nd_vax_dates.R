d5156a7436ce35772a8e17f36368167c25016a9a
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-11-30 10:26:53 +0000

    Update plot_2nd_vax_dates.R

--- analysis/eda_index_dates/plot_2nd_vax_dates.R
+++ analysis/eda_index_dates/plot_2nd_vax_dates.R
@@ -19,7 +19,8 @@ dir.create(images_dir, showWarnings = FALSE, recursive=TRUE)
 # read data for plotting
 data_plot <- readr::read_rds(
   here::here("output", "eda_index_dates", "data", "data_vax_plot.rds")
-)
+)#%>%
+# mutate(across(n, ~.x*100))
 
 # read elig_dates
 elig_dates <- readr::read_csv(here::here("output", "lib", "elig_dates.csv")) %>%
@@ -51,7 +52,7 @@ for (plot_date in as.character(sort(unique(data_plot$elig_date)))) {
     mutate(across(brand, 
                   ~factor(brand, 
                           levels = c("az", "pfizer"),
-                          labels = c("ChAdOx", "BNT162b2"))))
+                          labels = c("ChAdOx", "BNT162b2")))) 
   
   jcvi_groups <- elig_dates %>%
     filter(date %in% as.Date(plot_date)) %>%
@@ -73,102 +74,73 @@ for (plot_date in as.character(sort(unique(data_plot$elig_date)))) {
                   as.Date(plot_date) + weeks(16),
                   14) #14 days
   
-  caption <- "X-axes show 6 to 16 weeks after eligibility date. Bars are stacked by vaccine brand."
+  data_ma <- data %>%
+    group_by(region_0, brand) %>%
+    mutate(moving_average = stats::filter(
+      x = n,
+      filter = rep(1/l, l),
+      method = "convolution",
+      sides = 2)) %>% 
+    ungroup()
+  
 
-  # plot the data
-  plot_by_region <- data %>%
-    # calculate approx. total within region:brand group (approx as post-redaction)
-    group_by(region_0) %>%
-    mutate(n_region = scales::comma(sum(n), accuracy = 1)) %>%
+  
+  second_vax_period_dates[[i]] <- data_ma %>%
+    mutate(
+      dose_2_above_threshold = if_else(
+        moving_average > index_threshold,
+        dose_2,
+        NA_Date_
+      )) %>%
+    group_by(region_0, brand) %>%
+    mutate(
+      start_of_period = min(dose_2_above_threshold, na.rm = TRUE),
+      end_of_period = max(dose_2_above_threshold, na.rm = TRUE)
+      ) %>%
+    filter(dose_2 >= start_of_period, dose_2 <= end_of_period) %>%
+    mutate(n_in_period = sum(n)) %>%
     ungroup() %>%
-    mutate(across(region_0,
-                  ~str_replace(.x,
-                               "Yorkshire and The Humber",
-                               "Yorkshire & Humber"))) %>%
-    mutate(across(region_0, ~glue("{region_0} (n={n_region})"))) %>%
-    ggplot(aes(x = dose_2, y = n, fill = brand)) +
-    geom_bar(stat = "identity", position = "stack", width=1, alpha=0.9) +
+    distinct(region_0, brand, start_of_period, end_of_period, n_in_period) %>%
+    mutate(elig_date = as.Date(plot_date, format = "%Y-%m-%d"))
+    
+  plot_by_region <- ggplot(NULL, aes(x = dose_2)) +
+    geom_bar(data = data_ma %>% filter(brand == "ChAdOx"), 
+             aes(y = n, fill = "ChAdOx"),
+             stat = "identity",  alpha = 0.5) +
+    geom_bar(data = data_ma %>% filter(brand == "BNT162b2"), 
+             aes(y = n, fill = "BNT162b2"), 
+             stat = "identity", alpha = 0.5) +
+    geom_line(data = data_ma %>% filter(brand == "ChAdOx"), 
+              aes(y = moving_average, colour = "ChAdOx")) +
+    geom_line(data = data_ma %>% filter(brand == "BNT162b2"), 
+              aes(y = moving_average, colour = "BNT162b2")) +
+    geom_hline(yintercept = index_threshold, 
+               linetype = "dashed", colour = "grey") +
     facet_wrap(~ region_0, scales = "free_y") +
-    labs(x = "date of second vaccination", y = "number of patients",
-         title = title_string, subtitle = subtitle_string,
-         caption = str_wrap(caption, 120)) +
-    scale_fill_discrete(name = "vaccine brand") +
     scale_x_continuous(breaks = x_breaks,
                        labels = sapply(x_breaks, function(x) str_c(day(x), " ", month(x, label=TRUE)))) +
     scale_y_continuous(expand = expansion(mult = c(0,.05))) +
+    scale_fill_discrete(guide = FALSE) +
+    scale_colour_discrete(name = "brand") +
+    labs(x = "date of second vaccination", y = "number of patients",
+         title = title_string, subtitle = subtitle_string,
+         caption = caption_string) +
     theme_bw(base_size = 10) +
     theme(legend.position = "bottom",
+          axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)),
+          axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
           axis.text.x = element_text(size = 6),
           plot.caption = element_text(size = 10),
           plot.margin = margin(t=0.2, r=0.5, b=0.2, l=0.2, "cm")) +
     coord_cartesian(ylim = c(plot_threshold, NA))
-
+  # caption:
+  # X-axes restricted to 6 to 16 weeks after eligibility date.
+  # Bars show the number of individuals who received a second dose of the given brand of vaccine on the given date.
+  # Lines show the 7-day moving average (centred on day 4) of the numbers represented by the bars.
+  
   # save the plot
   ggsave(plot_by_region,
          filename = file.path(images_dir, glue("plot_by_region_{plot_date}.png")),
-         width=20, height=14, units="cm")
-  
-  data_plot_all <- data %>%
-    group_by(brand, dose_2) %>%
-    summarise(n_brand = sum(n), .groups = "keep") %>%
-    ungroup() %>%
-    group_by(brand) %>%
-    mutate(moving_average = stats::filter(
-      x = n_brand,
-      filter = rep(1/l, l),
-      method = "convolution",
-      sides = 2)) 
-  
-  second_vax_period_dates[[i]] <- data_plot_all %>%
-    # distinct(brand, dose_2, moving_average) %>%
-    filter(moving_average>index_threshold) %>%
-    group_by(brand) %>%
-    summarise(across(dose_2, list(min=min,max=max)), .groups = "keep") %>%
-    ungroup() %>%
-    mutate(elig_date = as.Date(plot_date, format = "%Y-%m-%d"))
-  
-  plot_all <- data_plot_all %>%
-    left_join(second_vax_period_dates[[i]], 
-              by = "brand") %>%
-    mutate(above_threshold = if_else(
-      moving_average > index_threshold,
-      dose_2,
-      NA_Date_)) %>%
-    group_by(brand) %>%
-    mutate(start = min(above_threshold, na.rm=TRUE),
-           end = max(above_threshold, na.rm=TRUE)) %>%
-    mutate(fill = if_else(dose_2 >= start & dose_2 <= end,
-                          TRUE, FALSE)) %>%
-    # mutate(keep_brand = any(fill)) %>%
-    ungroup() %>%
-    # filter(keep_brand) %>%
-    droplevels() %>%
-    ggplot() +
-    geom_bar(aes(x = dose_2, y = n_brand, fill = fill),
-             stat = "identity", position = "stack", width=1, alpha=0.9) +
-    geom_line(aes(x = dose_2, y = moving_average)) +
-    geom_hline(yintercept = index_threshold, linetype = "dashed") +
-    facet_wrap(~brand, scales = "free_y", nrow=2) +
-    labs(x = "date of second vaccination", y = "number of patients",
-         title = title_string, subtitle = subtitle_string,
-         caption = str_wrap(glue("Solid lines show {l}-day moving average of number of patients. The start of the \"second vaccination period\" is the date at which the solid line first crosses the dashed line (n = {index_threshold}), and the end of the study period is the last point at which the solid line crosses the dashed line. X-axes show 6 to 16 weeks after eligibility date."), 120)) +
-    scale_fill_manual(breaks = c(FALSE, TRUE), 
-                      values=c("#bdbdbd", "#31a354"),
-                      name = "Included in \"second vaccination period\":") +
-    scale_x_continuous(breaks = x_breaks,
-                       labels = sapply(x_breaks, function(x) str_c(day(x), " ", month(x, label=TRUE)))) +
-    scale_y_continuous(expand = expansion(mult = c(0,.05))) +
-    guides(fill = guide_legend(title.position = "top")) +
-    theme_bw(base_size = 10) +
-    theme(legend.position = "bottom",
-          axis.text.x = element_text(size = 6),
-          plot.caption = element_text(size = 10),
-          plot.margin = margin(t=0.2, r=0.5, b=0.2, l=0.2, "cm")) +
-    coord_cartesian(ylim = c(plot_threshold, NA))
-  
-  # save the plot
-  ggsave(plot_all,
-         filename = file.path(images_dir, glue("plot_all_{plot_date}.png")),
          width=20, height=14, units="cm")
     
   i <- i+1
