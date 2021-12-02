39eb98e78040a6a427ff97c82e5cebe45fe6d245
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-12-02 12:40:03 +0000

    Create dummy_data_covs.R

---
+++ analysis/dummy_data_covs.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(lubridate)
+library(glue)
+
+dummy_data <- arrow::read_feather(here::here("analysis", "lib", "dummy_data_vax.feather"))
+
+
+dummy_data_covs <- dummy_data %>%
+  select(patient_id, age_1, age_2, sex, elig_date, region_0) %>%
+  mutate(
+    # fill in these vars one study_definition_covs reviewed
+  )
+
+arrow::write_feather(dummy_data_covs, here::here("analysis", "lib", "dummy_data_covs.feather"))
+  
