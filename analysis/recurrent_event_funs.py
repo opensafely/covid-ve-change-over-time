9e43afd1c218ffec7c21bd21299a85d00c40228a
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-12-03 16:04:53 +0000

    before reverting to previous recurrent event functions

--- analysis/recurrent_event_funs.py
+++ analysis/recurrent_event_funs.py
@@ -21,7 +21,7 @@ def imd_k(K):
         }
     variables = {}
     for k in range(1, K+1):
-        variables.update(var_signature(f"imd_{k}", f"start_{k}_date"))
+        variables.update(var_signature(f"imd_{k}", f"start_1_date + {(k-1)*28} days"))
     return variables
 
 # region defined at each start_k_date
@@ -56,143 +56,172 @@ def region_k(K):
         variables.update(var_signature(f"region_{k}", f"start_1_date + {(k-1)*28} days"))
     return variables
 
+
+# bmi
+def most_recent_bmi_X(name, K):
+
+    def var_signature(name, k):
+        return {
+            name: patients.most_recent_bmi(
+                between=[f"start_1_date + {(k-1)*28 + 1} days", f"end_1_date + {k*28} days"],
+                minimum_age_at_measurement=16,
+                include_measurement_date=True,
+                date_format="YYYY-MM-DD",
+                return_expectations={
+                        "float": {"distribution": "normal", "mean": 28, "stddev": 8},
+                        "date": {"earliest": "2020-01-01", "latest": "2021-01-01"},
+                        "rate": "exponential_increase",
+                        "incidence": 0.01
+                },
+            )
+        }
+    variables={}
+    for i in range(1, K): # don't need to look between start_K and end_K so up to K-1 fine
+        variables.update(var_signature(f"{name}_{i}", i))
+    return variables
+
+
 # clinical events with codelist
-def with_these_clinical_events_date_X(name, codelist, index_date, n, return_expectations):
+def with_these_clinical_events_date_X(name, K, codelist):
     
-    def var_signature(name, on_or_after, codelist, return_expectations):
+    def var_signature(name, k, codelist):
         return {
             name: patients.with_these_clinical_events(
                     codelist,
                     returning="date",
-                    on_or_after=on_or_after,
+                    between=[f"start_1_date + {(k-1)*28 + 1} days", f"end_1_date + {k*28} days"],
                     date_format="YYYY-MM-DD",
-                    find_first_match_in_period=True,
-                    return_expectations=return_expectations
+                    find_last_match_in_period=True,
+                    return_expectations={
+                        "date": {"earliest": "2020-01-01", "latest": "2021-01-01"},
+                        "rate": "exponential_increase",
+                        "incidence": 0.01
+                    },
 	        ),
         }
-    variables=var_signature(f"{name}_1_date", index_date, codelist, return_expectations)
-    for i in range(2, n+1):
-        variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", codelist, return_expectations))
+    variables={}
+    for i in range(1, K): # don't need to look between start_K and end_K so up to K-1 fine
+        variables.update(var_signature(f"{name}_{i}_date", i, codelist))
     return variables
 
-# medications with codelist
-def with_these_medications_date_X(name, codelist, index_date, n, return_expectations):
+# # medications with codelist
+# def with_these_medications_date_X(name, codelist, index_date, n, return_expectations):
     
-    def var_signature(name, on_or_after, codelist, return_expectations):
-        return {
-            name: patients.with_these_medications(
-                    codelist,
-                    returning="date",
-                    on_or_after=on_or_after,
-                    date_format="YYYY-MM-DD",
-                    find_first_match_in_period=True,
-                    return_expectations=return_expectations
-	        ),
-        }
-    variables=var_signature(f"{name}_1_date", index_date, codelist, return_expectations)
-    for i in range(2, n+1):
-        variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", codelist, return_expectations))
-    return variables
+#     def var_signature(name, on_or_after, codelist, return_expectations):
+#         return {
+#             name: patients.with_these_medications(
+#                     codelist,
+#                     returning="date",
+#                     on_or_after=on_or_after,
+#                     date_format="YYYY-MM-DD",
+#                     find_first_match_in_period=True,
+#                     return_expectations=return_expectations
+# 	        ),
+#         }
+#     variables=var_signature(f"{name}_1_date", index_date, codelist, return_expectations)
+#     for i in range(2, n+1):
+#         variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", codelist, return_expectations))
+#     return variables
 
-# bmi
-def most_recent_bmi_X(name, index_date, n, return_expectations):
+# # bmi
+# def most_recent_bmi_X(name, index_date, n, return_expectations):
 
-    def var_signature(name, on_or_before, return_expectations):
-        return {
-            name: patients.most_recent_bmi(
-                on_or_before=on_or_before,
-                minimum_age_at_measurement=16,
-                include_measurement_date=True,
-                date_format="YYYY-MM-DD",
-                return_expectations=return_expectations
-            )
-        }
-    variables=var_signature(f"{name}_1", index_date, return_expectations)
-    for i in range(2, n+1):
-        variables.update(var_signature(f"{name}_{i}", f"{name}_{i-1}_date_measured - 1 day", return_expectations))
-    return variables
+#     def var_signature(name, on_or_before, return_expectations):
+#         return {
+#             name: patients.most_recent_bmi(
+#                 on_or_before=on_or_before,
+#                 minimum_age_at_measurement=16,
+#                 include_measurement_date=True,
+#                 date_format="YYYY-MM-DD",
+#                 return_expectations=return_expectations
+#             )
+#         }
+#     variables=var_signature(f"{name}_1", index_date, return_expectations)
+#     for i in range(2, n+1):
+#         variables.update(var_signature(f"{name}_{i}", f"{name}_{i-1}_date_measured - 1 day", return_expectations))
+#     return variables
 
-# covid test date
-def covid_test_date_X(name, index_date, n, test_result, return_expectations):
+# # covid test date
+# def covid_test_date_X(name, index_date, n, test_result, return_expectations):
     
-    def var_signature(name, on_or_after, test_result, return_expectations):
-        return {
-            name: patients.with_test_result_in_sgss(
-                pathogen="SARS-CoV-2",
-                test_result=test_result,
-                on_or_after=on_or_after,
-                find_first_match_in_period=True,
-                restrict_to_earliest_specimen_date=False,
-                returning="date",
-                date_format="YYYY-MM-DD",
-                return_expectations=return_expectations
-	        ),
-        }
-    variables = var_signature(f"{name}_1_date", index_date, test_result, return_expectations)
-    for i in range(2, n+1):
-        variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", test_result, return_expectations))
-    return variables
+#     def var_signature(name, on_or_after, test_result, return_expectations):
+#         return {
+#             name: patients.with_test_result_in_sgss(
+#                 pathogen="SARS-CoV-2",
+#                 test_result=test_result,
+#                 on_or_after=on_or_after,
+#                 find_first_match_in_period=True,
+#                 restrict_to_earliest_specimen_date=False,
+#                 returning="date",
+#                 date_format="YYYY-MM-DD",
+#                 return_expectations=return_expectations
+# 	        ),
+#         }
+#     variables = var_signature(f"{name}_1_date", index_date, test_result, return_expectations)
+#     for i in range(2, n+1):
+#         variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", test_result, return_expectations))
+#     return variables
 
-# emergency attendence
-def emergency_attendance_date_X(name, index_date, n, return_expectations):
+# # emergency attendence
+# def emergency_attendance_date_X(name, index_date, n, return_expectations):
     
-    def var_signature(name, on_or_after, return_expectations):
-        return {
-            name: patients.attended_emergency_care(
-                    returning="date_arrived",
-                    on_or_after=on_or_after,
-                    find_first_match_in_period=True,
-                    date_format="YYYY-MM-DD",
-                    return_expectations=return_expectations
-	        ),
-        }
-    variables = var_signature(f"{name}_1_date", index_date, return_expectations)
-    for i in range(2, n+1):
-        variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", return_expectations))
-    return variables
+#     def var_signature(name, on_or_after, return_expectations):
+#         return {
+#             name: patients.attended_emergency_care(
+#                     returning="date_arrived",
+#                     on_or_after=on_or_after,
+#                     find_first_match_in_period=True,
+#                     date_format="YYYY-MM-DD",
+#                     return_expectations=return_expectations
+# 	        ),
+#         }
+#     variables = var_signature(f"{name}_1_date", index_date, return_expectations)
+#     for i in range(2, n+1):
+#         variables.update(var_signature(f"{name}_{i}_date", f"{name}_{i-1}_date + 1 day", return_expectations))
+#     return variables
 
-# hospital admissions
-def admitted_date_X(
-    name, index_name, index_date, n, returning, 
-    with_these_diagnoses=None, 
-    with_admission_method=None, 
-    with_patient_classification=None, 
-    return_expectations=None
-):
-    def var_signature(
-        name, on_or_after, returning, 
-        with_these_diagnoses, 
-        with_admission_method, 
-        with_patient_classification, 
-        return_expectations
-    ):
-        return {
-            name: patients.admitted_to_hospital(
-                    returning = returning,
-                    on_or_after = on_or_after,
-                    find_first_match_in_period = True,
-                    date_format = "YYYY-MM-DD",
-                    with_these_diagnoses = with_these_diagnoses,
-                    with_admission_method = with_admission_method,
-                    with_patient_classification = with_patient_classification,
-                    return_expectations = return_expectations
-	        ),
-        }
-    variables = var_signature(
-        f"{name}_1_date", 
-        index_date, 
-        returning, 
-        with_these_diagnoses,
-        with_admission_method,
-        with_patient_classification,
-        return_expectations
-    )
-    for i in range(2, n+1):
-        variables.update(var_signature(
-            f"{name}_{i}_date", f"{index_name}_{i-1}_date + 1 day", returning, 
-            with_these_diagnoses,
-            with_admission_method,
-            with_patient_classification,
-            return_expectations
-        ))
-    return variables
+# # hospital admissions
+# def admitted_date_X(
+#     name, index_name, index_date, n, returning, 
+#     with_these_diagnoses=None, 
+#     with_admission_method=None, 
+#     with_patient_classification=None, 
+#     return_expectations=None
+# ):
+#     def var_signature(
+#         name, on_or_after, returning, 
+#         with_these_diagnoses, 
+#         with_admission_method, 
+#         with_patient_classification, 
+#         return_expectations
+#     ):
+#         return {
+#             name: patients.admitted_to_hospital(
+#                     returning = returning,
+#                     on_or_after = on_or_after,
+#                     find_first_match_in_period = True,
+#                     date_format = "YYYY-MM-DD",
+#                     with_these_diagnoses = with_these_diagnoses,
+#                     with_admission_method = with_admission_method,
+#                     with_patient_classification = with_patient_classification,
+#                     return_expectations = return_expectations
+# 	        ),
+#         }
+#     variables = var_signature(
+#         f"{name}_1_date", 
+#         index_date, 
+#         returning, 
+#         with_these_diagnoses,
+#         with_admission_method,
+#         with_patient_classification,
+#         return_expectations
+#     )
+#     for i in range(2, n+1):
+#         variables.update(var_signature(
+#             f"{name}_{i}_date", f"{index_name}_{i-1}_date + 1 day", returning, 
+#             with_these_diagnoses,
+#             with_admission_method,
+#             with_patient_classification,
+#             return_expectations
+#         ))
+#     return variables
