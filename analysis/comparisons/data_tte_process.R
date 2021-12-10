9f5e567b2b555228bc5e65368f5da6153c050830
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-12-10 17:00:01 +0000

    Update data_tte_process.R

--- analysis/comparisons/data_tte_process.R
+++ analysis/comparisons/data_tte_process.R
@@ -1,10 +1,59 @@
 
 
 ################################################################################
-b <- "BNT162b2"
 
-data_tte <- comparison_arms_data %>%
-  filter(brand %in% b) %>%
+## import command-line arguments ----
+args <- commandArgs(trailingOnly=TRUE)
+
+if(length(args)==0){
+  # use for interactive testing
+  removeobs <- FALSE
+  group <- "02"
+  
+} else{
+  removeobs <- TRUE
+  group <- args[[1]]
+}
+
+vax_brand <- "BNT162b2"
+
+outcomes <- c("postest", "covidadmitted", "coviddeath", "noncoviddeath", "death")
+
+data_covariates <- readr::read_rds(
+  here::here("output", glue("jcvi_group_{group}"), "data", "data_covariates.rds"))
+
+tte <- function(.data, var) {
+  .data %>% 
+    mutate()
+}
+
+data_tte <- data_covariates %>%
+  select(patient_id, 
+         elig_date, region, brand, arm, time_zero, end_fu_date, k,
+         coviddeath_date, death_date) %>%
+  left_join(
+    readr::read_rds(
+      here::here("output", glue("jcvi_group_{group}"),  "data", "data_long_postest_dates.rds")
+      ) %>% 
+      select(patient_id, postest_date = date),
+    by = "patient_id"
+  ) %>%
+  left_join(
+    readr::read_rds(
+      here::here("output", glue("jcvi_group_{group}"),  "data", "data_long_covidadmitted_dates.rds")
+    ) %>% 
+      select(patient_id, covidadmitted_date = date),
+    by = "patient_id"
+  ) %>%
+  group_by(brand, k) %>%
+  mutate(origin = min(time_zero)) %>%
+  ungroup() %>%
+  
+
+
+  
+  
+  filter(brand %in% vax_brand) %>%
   left_join(input_covs %>%
               select(patient_id, positive_test_date),
             by = "patient_id") %>%
