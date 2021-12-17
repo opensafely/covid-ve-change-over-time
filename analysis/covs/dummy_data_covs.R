3a1840dc5ec973db26a3eb00584dbdffb73d0681
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-12-17 09:24:57 +0000

    Update dummy_data_covs.R

--- analysis/covs/dummy_data_covs.R
+++ analysis/covs/dummy_data_covs.R
@@ -2,21 +2,28 @@ library(tidyverse)
 library(lubridate)
 library(glue)
 
-source(here::here("analysis", "lib", "dummy_data_functions.R"))
+################################################################################
+## preliminaries
 
-set.seed(5476)
+# read study parameters
+study_parameters <- readr::read_rds(
+  here::here("output", "lib", "study_parameters.rds"))
+
+# set seed so dummy data reproducible
+set.seed(study_parameters$seed)
 
 # the variables generated in grouping_variables.py are same as in dummy_data_vax
 dummy_data_vax <- arrow::read_feather(
   here::here("analysis", "vax", "dummy_data_vax.feather"))
 
-study_parameters <- readr::read_rds(
-  here::here("output", "lib", "study_parameters.rds"))
+# read functions for creating dummy data
+source(here::here("analysis", "lib", "dummy_data_functions.R"))
 
-# start and end dates
+# read start and end dates for second vax period
 start_dates <- readr::read_csv(here::here("output", "lib", "start_dates.csv"))
 end_dates <- readr::read_csv(here::here("output", "lib", "end_dates.csv"))
 
+# define function for translating the conditions from python to R
 translate_to_R <- function(.data) {
   .data %>%
     # add the quotations round elig_date (these weren't needed study_definition_covs.py)
@@ -28,11 +35,12 @@ translate_to_R <- function(.data) {
     mutate(across(condition, ~ str_replace_all(.x, "OR", "|"))) %>%
     mutate(across(condition, ~ str_replace(.x, "DEFAULT", "TRUE"))) 
 }
-# translate conditions to R
+# translate start and end date conditions to R
 start_dates <- start_dates %>% translate_to_R()
 end_dates <- end_dates %>% translate_to_R()
 
-# date vars (incidences are higher than reality to ensure code runs on smaller sample)
+# date vars 
+# set these to have occured ever during lifetime
 date_vars_ever <- c("chronic_cardiac_disease_date",
                     "heart_failure_date",
                     "other_heart_disease_date",
@@ -57,7 +65,7 @@ date_vars_ever <- c("chronic_cardiac_disease_date",
                     "dementia_date", 
                     "other_neuro_conditions_date", 
                     "psychosis_schiz_bipolar_date")
-
+# set these to have occured since start of pandemic
 date_vars_recent <- c("positive_test_0_date", 
                       "primary_care_covid_case_0_date", 
                       "primary_care_suspected_covid_0_date",
@@ -69,7 +77,10 @@ date_vars_recent <- c("positive_test_0_date",
                       "coviddeath_date", 
                       "dereg_date")
 
+################################################################################
+## generate dummy_data_covs
 
+# use variables from dummy_data_vax to start
 dummy_data_covs <- dummy_data_vax %>%
   select(patient_id, age_1, age_2, sex, jcvi_group, elig_date, region_0) %>%
   mutate(across(ends_with("_date"), as.Date)) %>%
@@ -84,8 +95,9 @@ dummy_data_covs <- dummy_data_vax %>%
     categories = end_dates$end_1_date,
     conditions = end_dates$condition
   ) %>%
+  # indicator for flu vaccine in past 5 years
   var_binary(name = flu_vaccine, incidence = 0.3) %>%
-  # date vars  ever
+  # date vars ever
   bind_cols(
     pmap(
       list(a = date_vars_ever, 
@@ -97,6 +109,7 @@ dummy_data_covs <- dummy_data_vax %>%
           incidence = b,
           keep_vars = FALSE
         ))) %>%
+  # date vars recent
   bind_cols(
     pmap(
       list(a = date_vars_recent, 
@@ -110,13 +123,17 @@ dummy_data_covs <- dummy_data_vax %>%
                latest="2021-12-31",
                keep_vars = FALSE
              ))) %>%
+  # add death_date if coviddeath_date
   mutate(across(death_date, 
                 ~if_else(
                   !is.na(coviddeath_date), 
                   coviddeath_date,
                   NA_Date_))) %>%
+  # add recurrent bmi vars
   bind_cols(vars_bmi_recurrent(.data = .)) %>%
+  # add K region and imd vars
   bind_cols(vars_region_and_imd_recurrent(.data = ., K=K)) %>%
+  # add recurrent shielded vars
   bind_cols(
     var_date_recurrent(
       .data = ., 
@@ -129,6 +146,7 @@ dummy_data_covs <- dummy_data_vax %>%
       name_string = "nonshielded", 
       incidence = 0.1,
       r = study_parameters$recur_shielded)) %>%
+  # add recurrent hospital admission vars
   bind_cols(
     var_date_recurrent(
       .data = ., 
@@ -141,12 +159,14 @@ dummy_data_covs <- dummy_data_vax %>%
       name_string = "admitted_unplanned_infectious", 
       incidence = 0.1,
       r = study_parameters$recur_admissions)) %>%
+  # add dob
   # all 1st of the month as dob YYYY-MM in OpenSAFELY
   mutate(
     dob = as.POSIXct(sample(
       x = seq(as.Date("1930-01-01"), as.Date("2006-01-01"), by = "1 month"), 
       size = nrow(.),
       replace = TRUE))) %>%
+  # add efi
   mutate(
     efi = if_else(
       rbernoulli(n = nrow(.), p = 0.99),
@@ -154,6 +174,7 @@ dummy_data_covs <- dummy_data_vax %>%
       NA_real_)) %>%
   mutate(across(contains("_date"), as.POSIXct)) 
 
+# add recurrent hospital discharge vars based on admission dates
 dummy_data_covs <- dummy_data_covs %>%
   bind_cols(
     lapply(
@@ -170,5 +191,7 @@ dummy_data_covs <- dummy_data_covs %>%
     )) %>%
   droplevels()
 
-
-arrow::write_feather(dummy_data_covs, here::here("analysis", "covs", "dummy_data_covs.feather"))
+# write data to feather file
+arrow::write_feather(
+  dummy_data_covs, 
+  here::here("analysis", "covs", "dummy_data_covs.feather"))
