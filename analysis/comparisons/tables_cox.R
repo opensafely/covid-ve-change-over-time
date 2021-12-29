ee35d6a4ebbdae24b82cef5ef527f959f13d9a67
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-12-29 14:20:19 +0000

    Update tables_cox.R

--- analysis/comparisons/tables_cox.R
+++ analysis/comparisons/tables_cox.R
@@ -13,14 +13,32 @@ if(length(args)==0){
   # use for interactive testing
   removeobs <- FALSE
   group <- "02"
-  outcome <- "postest"
   
 } else{
   removeobs <- TRUE
   group <- args[[1]]
-  outcome <- args[[2]]
 }
 
 ################################################################################
 
 fs::dir_create(here::here("output", glue("jcvi_group_{group}"), "tables"))
+
+second_vax_period_dates <- readr::read_rds(
+  here::here("output", "lib", "second_vax_period_dates.rds")) %>%
+  filter(jcvi_group %in% group, include) %>%
+  distinct(brand, n_comparisons)
+
+
+for (b in as.character(unique(second_vax_period_dates$brand))) {
+  
+  model_tidy <- lapply(
+    
+  )
+  
+  model_tidy <- readr::read_rds(
+    here::here("output", glue("jcvi_group_{group}"), "models", glue("{b}_{outcome}_modelcox_tidy.rds"))) 
+  
+  model_tidy <- readr::read_rds(
+    here::here("output", glue("jcvi_group_{group}"), "models", glue("{b}_{outcome}_modelcox_tidy.rds"))) 
+  
+}
