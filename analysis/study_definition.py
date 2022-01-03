050ecc0b3d3f8960002bae8d5a2f929b555cd2bb
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-01-03 19:51:13 +0000

    add efi and flu back

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -651,46 +651,46 @@ study=StudyDefinition(
         },
     ),
 
-    # # flu vaccine in the 5 years before elig_date
-    # flu_vaccine=patients.satisfying(
-    #     """
-    #     flu_vaccine_tpp_table>0 OR
-    #     flu_vaccine_med>0 OR
-    #     flu_vaccine_clinical>0
-    #     """,
+    # flu vaccine in the 5 years before elig_date
+    flu_vaccine=patients.satisfying(
+        """
+        flu_vaccine_tpp_table>0 OR
+        flu_vaccine_med>0 OR
+        flu_vaccine_clinical>0
+        """,
         
-    #     flu_vaccine_tpp_table=patients.with_tpp_vaccination_record(
-    #         target_disease_matches="INFLUENZA",
-    #         between=["elig_date - 5 years", "elig_date"], 
-    #         returning="binary_flag",
-    #     ),
+        flu_vaccine_tpp_table=patients.with_tpp_vaccination_record(
+            target_disease_matches="INFLUENZA",
+            between=["elig_date - 5 years", "elig_date"], 
+            returning="binary_flag",
+        ),
         
-    #     flu_vaccine_med=patients.with_these_medications(
-    #         flu_med_codes,
-    #         between=["elig_date - 5 years", "elig_date"], 
-    #         returning="binary_flag",
-    #     ),
-    #     flu_vaccine_clinical=patients.with_these_clinical_events(
-    #         flu_clinical_given_codes,
-    #         ignore_days_where_these_codes_occur=flu_clinical_not_given_codes,
-    #         between=["elig_date - 5 years", "elig_date"], 
-    #         returning="binary_flag",
-    #     ),
-    #     return_expectations={"incidence": 0.5, },
-    # ),
+        flu_vaccine_med=patients.with_these_medications(
+            flu_med_codes,
+            between=["elig_date - 5 years", "elig_date"], 
+            returning="binary_flag",
+        ),
+        flu_vaccine_clinical=patients.with_these_clinical_events(
+            flu_clinical_given_codes,
+            ignore_days_where_these_codes_occur=flu_clinical_not_given_codes,
+            between=["elig_date - 5 years", "elig_date"], 
+            returning="binary_flag",
+        ),
+        return_expectations={"incidence": 0.5, },
+    ),
 
-    # # electronic frailty index
-    # # date hard-coded because there are few dates available for efi
-    # efi=patients.with_these_decision_support_values(
-    #     algorithm="electronic_frailty_index",
-    #     on_or_before="elig_date + 56 days", 
-    #     find_last_match_in_period=True,
-    #     returning="numeric_value",
-    #     return_expectations={
-    #         "float": {"distribution": "normal", "mean": 0.20, "stddev": 0.09},
-    #         "incidence": 0.99
-    #     },
-    # ),
+    # electronic frailty index
+    # date hard-coded because there are few dates available for efi
+    efi=patients.with_these_decision_support_values(
+        algorithm="electronic_frailty_index",
+        on_or_before="elig_date + 56 days", 
+        find_last_match_in_period=True,
+        returning="numeric_value",
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 0.20, "stddev": 0.09},
+            "incidence": 0.99
+        },
+    ),
     
     # dates of shielding codes
     shielded_0_date=patients.with_these_clinical_events(
