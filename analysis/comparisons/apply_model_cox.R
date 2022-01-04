338bd51dbb03a221c867eb032ec6bf7b541303ec
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-01-04 13:58:27 +0000

    Update apply_model_cox.R

--- analysis/comparisons/apply_model_cox.R
+++ analysis/comparisons/apply_model_cox.R
@@ -1,11 +1,17 @@
 ################################################################################
 
 # This script:
+# applies the cox model for a given comparison and outcome
 
 
 ################################################################################
 library(tidyverse)
+library(fastDummies)
 library(glue)
+library(survival)
+library(broom)
+library(broom.helpers)
+
 ## import command-line arguments ----
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -22,9 +28,7 @@ if(length(args)==0){
 ################################################################################
 # read data
 
-fs::dir_create(here::here("output", glue("jcvi_group_{group}"), "models"))
-
-source(here::here("analysis", "lib", "model_cox.R"))
+fs::dir_create(here::here("output", "models"))
 
 model_varlist <- readr::read_rds(
   here::here("output", "lib", "model_varlist.rds")
@@ -78,8 +82,6 @@ if (comparison %in% c("BNT162b2", "ChAdOx")) {
 data_strata <- data %>% 
   mutate(strata_var = factor(str_c(jcvi_group, elig_date, region, sep = ", ")))
 
-
-
 ################################################################################
 
 events_per_personyears <- function(strata) {
@@ -108,10 +110,6 @@ events_per_personyears <- function(strata) {
 
 ################################################################################
 
-for (s in levels(data_strata$strata_var)) {
-  print(events_per_personyears(s))
-}
-
 capture.output(
   for (s in levels(data_strata$strata_var)) {
     print(events_per_personyears(s))
@@ -130,7 +128,132 @@ data_cox <- data_strata %>%
   mutate(across(starts_with("comparison"),
                 ~ if_else(arm %in% trt,
                           .x, 0L))) %>%
-  mutate(strata_var = factor(str_c(jcvi_group, elig_date, region, sep = ", ")))
+  rename_at(vars(starts_with("comparison")), ~str_c(.x, "_", str_remove(trt, "_vax")))
+
+################################################################################
+# define formulas
+
+comparisons <- data_cox %>% select(starts_with("comparison")) %>% names()
+
+formula_unadj <- as.formula(str_c(
+  "Surv(tstart, tstop, status, type = \"counting\") ~ ",
+  str_c(comparisons, collapse = " + "),
+  " + strata(strata_var)"))
+formula_demog <- . ~ . + age_band + sex + imd + ethnicity
+formula_clinical <- . ~ . +
+  
+  bmi +
+  heart_failure +
+  other_heart_disease +
+  
+  dialysis +
+  diabetes +
+  chronic_liver_disease +
+  
+  current_copd +
+  #cystic_fibrosis +
+  other_respiratory +
+  
+  lung_cancer +
+  haematological_cancer +
+  cancer_excl_lung_and_haem +
+  
+  any_immunosuppression +
+  
+  dementia +
+  other_neuro_conditions +
+  
+  ld_inc_ds_and_cp +
+  psychosis_schiz_bipolar +
+  
+  multimorb +
+  
+  shielded +
+  
+  flu_vaccine +
+  
+  efi
+
+
+model_names = c(
+  "0" = "Unadjusted",
+  "1" = "Adjusting demographics",
+  "2" = "Adjusting for demographics + clinical"
+)
+
+formula_cox_0 <- formula_unadj
+formula_cox_1 <- formula_unadj %>% update(formula_demog)
+formula_cox_2 <- formula_unadj %>% update(formula_demog) %>% update(formula_clinical)
+
+################################################################################
+# define model
+
+cox_model <- function(
+  number, 
+  formula_cox,
+  filename_prefix
+) {
+  
+  # so that when cox_model is run in lapply the options are printed after each
+  # evaluation and not all at the end
+  op_warn <- options("warn")
+  on.exit(options(op_warn))
+  
+  options(warn=1)
+  
+  number <- as.character(number)
+  
+  model_name <- model_names[number]
+  
+  opt_control <- coxph.control(iter.max = 30)
+  
+  cat(glue("...... fitting model {number} ......"), "\n")
+  cat(glue("{model_name}"), "\n")
+  timetofit <- system.time((
+    coxmod <- coxph(
+      formula = formula_cox,
+      data = data_cox,
+      robust = TRUE,
+      id = patient_id,
+      na.action = "na.fail",
+      control = opt_control)
+  ))
+  
+  # process model (as having issues with broom tidy)
+  coxmod_summary <- as_tibble(
+    summary(coxmod)$conf.int, 
+    rownames = "term"
+  ) %>% 
+    select(term,
+           estimate = `exp(coef)`, 
+           lower = `lower .95`, 
+           upper = `upper .95`) %>%
+    mutate(model = number) 
+  
+  
+  glance <-
+    broom::glance(coxmod) %>%
+    add_column(
+      model = number,
+      convergence = coxmod$info[["convergence"]],
+      ram = format(object.size(coxmod), units="GB", standard="SI", digits=3L),
+      .before = 1
+    ) %>%
+    mutate(across(
+      model,
+      factor, levels = names(model_names), labels = model_names)) %>%
+    # add output of system.time
+    bind_cols(as_tibble(t(as.matrix(timetofit))))
+  
+  coxmod$data <- NULL
+  
+  readr::write_rds(
+    coxmod,
+    here::here("output", "models", glue("{filename_prefix}_model{number}.rds")), 
+    compress="gz")
+  
+  lst(glance = glance, summary = coxmod_summary)
+}
 
 ################################################################################
 
@@ -159,7 +282,7 @@ model_summary <- bind_rows(
   mutate(outcome = outcome)
 readr::write_rds(
   model_summary,
-  here::here("output", glue("jcvi_group_{group}"), "models", glue("{b}_{outcome}_modelcox_summary.rds"))) 
+  here::here("output", "models", glue("{comparison}_{outcome}_modelcox_summary.rds"))) 
 
 ### postprocessing using broom (may be unreliable)
 # combine results
@@ -174,5 +297,5 @@ model_glance <- bind_rows(
 
 readr::write_csv(
   model_glance,
-  here::here("output", glue("jcvi_group_{group}"), "models", glue("{b}_{outcome}_modelcox_glance.csv"))) 
+  here::here("output", "models", glue("{comparison}_{outcome}_modelcox_glance.csv"))) 
 
