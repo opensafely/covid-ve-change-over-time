68e502dccb319c65688f88716cd8ccdf768d97fc
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-01-08 12:20:17 +0000

    typo

--- analysis/comparisons/data_tte_process.R
+++ analysis/comparisons/data_tte_process.R
@@ -20,7 +20,7 @@ for (subgroup in c("02", "03-10", "11-12")) {
     comparisons <- "BNT162b2"
   } else if (subgroup == "03-10") {
     jcvi_groups <- c("03", "04", "05", "06", "07", "08", "09", "10")
-    comparison %in% c("BNT162b2", "ChAdOx", "both")
+    comparisons <- c("BNT162b2", "ChAdOx", "both")
   } else if (subgroup == "11-12") {
     jcvi_groups <- c("11", "12")
     comparisons <- "BNT162b2"
