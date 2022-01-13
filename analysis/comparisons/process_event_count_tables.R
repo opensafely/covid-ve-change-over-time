63bf14844a681632a18afc2ef10bce1c84b8e8bf
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-01-13 12:53:44 +0000

    Create process_event_count_tables.R

---
+++ analysis/comparisons/process_event_count_tables.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(glue)
+
+# read table_events
+table_events <- readr::read_rds(
+  here::here("output", "tte", "tables", glue("event_counts.rds")))
+
+# read subgroups
+subgroups <- readr::read_rds(
+  here::here("output", "lib", "subgroups.rds"))
+subgroups <- c(subgroups, "all")
+
+# read outcomes
+outcomes <- readr::read_rds(
+  here::here("output", "lib", "outcomes.rds"))
+
+
+process_tables <- function(
+  .data
+) {
+  
+  # bind across outcomes
+  table <- bind_rows(.data)
+  
+  arms <- names(table)[2:3]
+  if ("unvax" %in% arms) {
+    comparison <- arms[1]
+  } else {
+    comparison <- "both"
+  }
+  
+  # define subgroup
+  subgroup <- unique(table$subgroup)
+  subgroup_label <- which(subgroups == subgroup)
+  
+  # save data for generating tables for paper
+  readr::write_csv(
+    table,
+    here::here("output", "tte", "tables", glue("events_{comparison}_{subgroup_label}.csv")))
+  
+  # create tidy output for data checking
+  table_list <- table %>% 
+    mutate(across(outcome, factor, levels = outcomes)) %>%
+    select(-subgroup) %>%
+    group_split(outcome) %>%
+    as.list()
+  
+  # write tidy output to txt
+  capture.output(
+    lapply(
+      table_list,
+      function(x) {
+        outcome <-  unique(x$outcome)
+        x %>%
+          select(-outcome) %>%
+          kableExtra::kable(
+            "pipe",
+            caption = glue("subgroup = {subgroup}, outcome = {outcome}")
+          )
+      }
+    ),
+    file = here::here("output", "tte", "tables", glue("tidy_events_{comparison}_{subgroup_label}.txt")),
+    append=FALSE
+  )
+  
+}
+
+
+lapply(
+  seq_along(table_events),
+  function(x)
+    lapply(
+      seq_along(table_events[[x]]),
+      function(y)
+        process_tables(table_events[[x]][[y]])
+    )
+    
+)
+
+
