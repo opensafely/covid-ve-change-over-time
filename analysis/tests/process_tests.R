8ffdbf1dff42a49f2550e5b906537387d7b40225
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-01 11:45:57 +0000

    Update process_tests.R

--- analysis/tests/process_tests.R
+++ analysis/tests/process_tests.R
@@ -1,7 +1,18 @@
+################################################################################
 # process tests data
 library(tidyverse)
 library(lubridate)
 
+################################################################################
+## source functions
+source(here::here("analysis", "lib", "data_properties.R"))
+
+################################################################################
+## create folders for outputs
+fs::dir_create(here::here("output", "tests", "images"))
+fs::dir_create(here::here("output", "tests", "tables"))
+
+################################################################################
 data_tests <- arrow::read_feather(
   file = here::here("output", "input_tests.feather")) %>%
   mutate(across(c(contains("_date")), 
@@ -15,3 +26,26 @@ readr::write_rds(
   here::here("output", "data", "data_tests.rds"),
   compress = "gz"
 )
+
+################################################################################
+# tabulate all vars
+data_properties(
+  data = data_tests,
+  path = file.path("output", "tests", "tables")
+)
+
+################################################################################
+# plot distibution of coviariates
+data_tests %>%
+  select(patient_id, 
+         pre=covid_test_pre_elig_n,
+         post=covid_test_post_elig_n) %>%
+  mutate(both = pre + post) %>%
+  pivot_longer(cols = -patient_id) %>%
+  ggplot(aes(x = value)) +
+  geom_bar() +
+  facet_wrap(~ name, scales = "free") +
+  theme(legend.position = "bottom")
+ggsave(
+  filename = here::here("output", "tests", "images", "covariate_distribution.png"),
+  width=20, height=15, units="cm")
