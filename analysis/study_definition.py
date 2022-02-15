1db034805c1455f32e6960989f682fbb71223800
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-15 14:41:41 +0000

    remove recurrent events from study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -206,9 +206,6 @@ study=StudyDefinition(
     # as IMD and region are all defined on a given date, these can only be updated at elig_date + 8 weeks... 
     # this is the ealiest possible date for start_1 as:
     # elig_date + 6 weeks is earliest possible 2nd vax date, + 2 weeks for start of comparison 1
-    
-    # BMI will be derived as a sequence
-    # all other covariates are "ever", so can just find the earliest ate before end_K
 
     #############################
     ### DEMOGRAPHIC VARIABLES ###
@@ -265,32 +262,15 @@ study=StudyDefinition(
     # # ### CLINICAL VARIABLES ###
     # # ##########################
 
-    # 10 most recent bmi recordings before end_K
-    # bmi_1 is most recent, bmi_2 second most recent etc.
-    # elig_date + 16 weeks (latest 2nd vax) + 2 weeks (latest start_1) + (k+1)*28 days (latest end_k)
-    **most_recent_bmi_X(
-        name="bmi",
-        n=study_parameters["recur_bmi"],
-        index_date=f"elig_date + {112 + 14 + (max_comparisons + 1)*28} days",
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-            "incidence": 0.80,
-        },
-    ),
+    # "ever" variables only, others defined in study_definition_tests
 
-    # chronic caridac disease
-    chronic_cardiac_disease_date=patients.with_these_clinical_events(
-        chronic_cardiac_disease_codes,
+    # chronic heart disease
+    chd_date=patients.with_these_clinical_events(
+        chd_primis,
         returning="date",
         date_format="YYYY-MM-DD",
         on_or_before=f"elig_date + {112 + 14 + (max_comparisons + 1)*28} days",
         find_first_match_in_period=True,
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "rate": "exponential_increase",
-            "incidence": 0.01
-        },
     ),
    
     # heart failure
@@ -671,71 +651,9 @@ study=StudyDefinition(
         return_expectations={"incidence": 0.5, },
     ),
     
-    # dates of shielding codes
-    shielded_0_date=patients.with_these_clinical_events(
-        shield_primis,
-        returning="date",
-        date_format="YYYY-MM-DD",
-        on_or_before="elig_date + 56 days",
-        find_last_match_in_period=True,
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "rate": "exponential_increase",
-            "incidence": 0.01
-        },
-    ),
-    **with_these_clinical_events_date_X(
-        name="shielded",
-        n=study_parameters["recur_shielded"],
-        index_date="elig_date + 57 days",
-        codelist=shield_primis,
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "rate": "uniform",
-            "incidence": 0.01,
-        },
-    ),
-    
-    # dates of non shielding codes
-    nonshielded_0_date=patients.with_these_clinical_events(
-        nonshield_primis,
-        returning="date",
-        date_format="YYYY-MM-DD",
-        on_or_before="elig_date + 56 days",
-        find_last_match_in_period=True,
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "rate": "exponential_increase",
-            "incidence": 0.01
-        },
-    ),
-    **with_these_clinical_events_date_X(
-        name="nonshielded",
-        n=study_parameters["recur_shielded"],
-        index_date="elig_date + 57 days",
-        codelist=nonshield_primis,
-        return_expectations={
-            "date": {"earliest": start_date, "latest": end_date},
-            "rate": "uniform",
-            "incidence": 0.01,
-        },
-    ),
-
     ##############
     ### EVENTS ###
     ##############
-
-    # covid test
-    **covid_test_date_X(
-        name = "covid_test",
-        index_date = "elig_date + 42 days",
-        n = 10,
-        test_result="any",
-        return_expectations = {
-            "date": {"earliest": "2021-01-01",  "latest" : "2021-11-30"},
-            "rate": "exponential_increase",
-        },
-    ),
     
     # positive covid test
     positive_test_0_date=patients.with_test_result_in_sgss(
@@ -783,89 +701,6 @@ study=StudyDefinition(
             "incidence": 0.01
         },
     ),
-    
-    # # unplanned hospital admission
-    # admitted_unplanned_0_date=patients.admitted_to_hospital(
-    #     returning="date_admitted",
-    #     on_or_before="elig_date + 56 days",
-    #     find_first_match_in_period=True,
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     date_format="YYYY-MM-DD",
-    # ),
-    #  **admitted_date_X(
-    #     name = "admitted_unplanned",
-    #     n = study_parameters["recur_admissions"],
-    #     index_name = "admitted_unplanned",
-    #     index_date = "elig_date + 57 days",
-    #     returning="date_admitted",
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    # ),
-    
-    # discharged_unplanned_0_date=patients.admitted_to_hospital(
-    #     returning="date_discharged",
-    #     on_or_before="elig_date + 56 days",
-    #     find_first_match_in_period=True,
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     date_format="YYYY-MM-DD",
-    # ), 
-    # **admitted_date_X(
-    #     name = "discharged_unplanned",
-    #     n = study_parameters["recur_admissions"],
-    #     index_name = "admitted_unplanned",
-    #     index_date = "elig_date + 57 days",
-    #     returning="date_admitted",
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    # ),
-   
-    # # unplanned infectious hospital admission
-    # admitted_unplanned_infectious_0_date=patients.admitted_to_hospital(
-    #     returning="date_admitted",
-    #     on_or_before="elig_date + 56 days",
-    #     find_first_match_in_period=True,
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     with_these_diagnoses = ICD10_I_codes,
-    #     date_format="YYYY-MM-DD",
-    #     return_expectations={
-    #         "date": {"earliest": start_date, "latest": end_date},
-    #         "rate": "uniform",
-    #         "incidence": 0.05,
-    #     },
-    # ),
-    # **admitted_date_X(
-    #     name = "admitted_unplanned_infectious",
-    #     n = study_parameters["recur_admissions"],
-    #     index_name = "admitted_unplanned_infectious",
-    #     index_date = "elig_date + 57 days",
-    #     returning="date_admitted",
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     with_these_diagnoses = ICD10_I_codes,
-    # ),
-   
-    # discharged_unplanned_infectious_0_date=patients.admitted_to_hospital(
-    #     returning="date_discharged",
-    #     on_or_before="elig_date + 56 days",
-    #     find_first_match_in_period=True,
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     with_these_diagnoses = ICD10_I_codes,
-    #     date_format="YYYY-MM-DD",
-    # ), 
-    # **admitted_date_X(
-    #     name = "discharged_unplanned_infectious",
-    #     n = study_parameters["recur_admissions"],
-    #     index_name = "admitted_unplanned_infectious",
-    #     index_date = "elig_date + 57 days",
-    #     returning="date_discharged",
-    #     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"],
-    #     with_patient_classification = ["1"],
-    #     with_these_diagnoses = ICD10_I_codes,
-    # ),
     
     # covid hospital adamission
     covidadmitted_0_date=patients.admitted_to_hospital(
