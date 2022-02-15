134cfb4deb39a03b744aaac7df0cb89ae30f4ed9
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-15 21:22:34 +0000

    Create create_study_definitions.sh

---
+++ analysis/create_study_definitions.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Run this script to create a study_definition for each k
+
+for i in {1..6}; do
+sed -e "s;%placeholder_k%;$i;g" ./analysis/study_definition_k.py > ./analysis/study_definition_$i.py;
+done;
