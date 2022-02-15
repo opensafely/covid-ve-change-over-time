979d13efe41abf0e09c96c8a128149198598569d
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-15 14:41:55 +0000

    add recurrent events to study_definition_tests.py

--- analysis/study_definition_tests.py
+++ analysis/study_definition_tests.py
@@ -22,111 +22,8 @@ end_date=study_parameters["end_date"] # latest date of data
 import numpy as np
 np.random.seed(study_parameters["seed"])
 
-# recurrent var functions
-# count tests in K comparison periods
-def covid_test_k_n(K, test_result, return_expectations):
-    
-    def var_signature(name, lower, upper, test_result, return_expectations):
-        return {
-            name: patients.with_test_result_in_sgss(
-                pathogen="SARS-CoV-2",
-                test_result=test_result,
-                between=[lower, upper],
-                restrict_to_earliest_specimen_date=False,
-                returning="number_of_matches_in_period",
-                return_expectations=return_expectations
-	        ),
-        }
-    variables = dict()
-    for i in range(1, K+1): 
-        variables.update(var_signature(
-            f"{test_result}_test_{i}_n", 
-            f"start_1_date + {(i-1)*28 + 1} days", 
-            f"end_1_date + {(i-1)*28} days", 
-            test_result, 
-            return_expectations))
-    return variables
-
-# date of first test in K comparison periods
-def covid_test_k_date(K, test_result, return_expectations):
-    
-    def var_signature(name, lower, upper, test_result, return_expectations):
-        return {
-            name: patients.with_test_result_in_sgss(
-                pathogen="SARS-CoV-2",
-                test_result=test_result,
-                between=[lower, upper],
-                restrict_to_earliest_specimen_date=False,
-                find_first_match_in_period=True,
-                returning="date",
-                date_format = "YYYY-MM-DD",
-                return_expectations=return_expectations
-	        ),
-        }
-    variables = dict()
-    for i in range(1, K+1): 
-        variables.update(var_signature(
-            f"{test_result}_test_{i}_date", 
-            f"start_1_date + {(i-1)*28 + 1} days", 
-            f"end_1_date + {(i-1)*28} days", 
-            test_result, 
-            return_expectations))
-    return variables
-
-### pregnancy variables (derived in this script rather than study_definition.py as needed start_1_date)
-# date of last pregnancy code in 36 weeks before start of comparison follow-up
-def preg_36wks_k_date(K):
-
-    def var_signature(name, lower, upper):
-        return {
-            name: patients.with_these_clinical_events(
-                preg_primis,
-                returning="date",
-                find_last_match_in_period=True,
-                between=[lower, upper],
-                date_format="YYYY-MM-DD"),
-        }
-    variables = dict()
-    for i in range(1, K+1): 
-        variables.update(var_signature(
-            name=f"preg_36wks_{i}_date",
-            lower=f"start_1_date - {252 - (i-1)*28} days", 
-            upper=f"start_1_date + {(i-1)*28} days"))
-    return variables
-
-# date of last delivery code recorded in 36 weeks before start of comparison follow-up
-def pregdel_pre_k_date(K):
-
-    def var_signature(name, lower, upper):
-        return {
-            name: patients.with_these_clinical_events(
-            pregdel_primis,
-            returning="date",
-            find_last_match_in_period=True,
-            between=[lower, upper],
-            date_format="YYYY-MM-DD"),
-        }
-    variables = dict()
-    for i in range(1, K+1): 
-        variables.update(var_signature(
-            name=f"pregdel_pre_{i}_date",
-            lower=f"start_1_date - {252 - (i-1)*28} days", 
-            upper=f"start_1_date + {(i-1)*28} days"))
-    return variables
-
-# pregnancy vairable that updates at start of each comparison
-def preg_k(K):
-
-    def var_signature(name, condition):
-        return {
-            name: patients.satisfying(condition),
-        }
-    variables = dict()
-    for i in range(1, K+1): 
-        variables.update(var_signature(
-            name=f"preg_{i}", 
-            condition=f"(preg_36wks_{i}_date) AND (pregdel_pre_{i}_date <= preg_36wks_{i}_date OR NOT pregdel_pre_{i}_date)"))
-    return variables
+# import recurring event functions
+from recurrent_event_funs import *
 
 ###
 study=StudyDefinition(
@@ -200,17 +97,17 @@ study=StudyDefinition(
     
     ### covid tests as outcomes
     # number of covid tests in each 28-day period
-    **covid_test_k_n(
-        K=max_comparisons,
-        test_result="any",
-        return_expectations={"int" : {"distribution": "poisson", "mean": 2}, "incidence" : 0.6}
-    ),
-    # number of positive covid tests in each 28-day period
-    **covid_test_k_n(
-        K=max_comparisons,
-        test_result="positive",
-        return_expectations={"int" : {"distribution": "poisson", "mean": 0.5}, "incidence" : 0.6}
-    ),
+    # **covid_test_k_n(
+    #     K=max_comparisons,
+    #     test_result="any",
+    #     return_expectations={"int" : {"distribution": "poisson", "mean": 2}, "incidence" : 0.6}
+    # ),
+    # # number of positive covid tests in each 28-day period
+    # **covid_test_k_n(
+    #     K=max_comparisons,
+    #     test_result="positive",
+    #     return_expectations={"int" : {"distribution": "poisson", "mean": 0.5}, "incidence" : 0.6}
+    # ),
     # first covid test in each 28-day period
     **covid_test_k_date(
         K=max_comparisons,
@@ -218,9 +115,51 @@ study=StudyDefinition(
         return_expectations={"date": {"earliest": start_date, "latest": end_date}}
     ),
 
+    #### clinical covariates
     # indicator for pregnancy at start of follow-up for comparison k
-    **preg_36wks_k_date(K=max_comparisons),
-    **pregdel_pre_k_date(K=max_comparisons),
-    **preg_k(K=max_comparisons)
+    **preg_36wks_k_date(max_comparisons),
+    **pregdel_pre_k_date(max_comparisons),
+    **preg_k(max_comparisons),
 
+    bmi_0=patients.most_recent_bmi(
+                on_or_before=f"start_1_date",
+                minimum_age_at_measurement=16,
+                include_measurement_date=True,
+                date_format="YYYY-MM-DD"),
+    **most_recent_bmi_k(max_comparisons),
+    bmi_0_stage=patients.with_these_clinical_events(
+                bmi_stage_primis,
+                on_or_before=f"start_1_date",
+                returning="category",
+                include_date_of_match=True,
+                date_format="YYYY-MM-DD",
+                find_last_match_in_period=True
+            ),
+    **most_recent_bmi_stage_k(max_comparisons),
+
+    # date of most recent shielding recording before each comparison start date
+    shielded_0_date=patients.with_these_clinical_events(
+        shield_primis,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        on_or_before="start_1_date",
+        find_last_match_in_period=True,
+    ),
+    **with_these_clinical_events_date_k(
+        K=max_comparisons,
+        name="shielded",
+        codelist=shield_primis
+    ),
+    nonshielded_0_date=patients.with_these_clinical_events(
+        shield_primis,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        on_or_before="start_1_date",
+        find_last_match_in_period=True,
+    ),
+    **with_these_clinical_events_date_k(
+        K=max_comparisons,
+        name="nonshielded",
+        codelist=nonshield_primis
+    ),
 )
