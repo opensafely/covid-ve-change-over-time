78f3c4bb5550318a599edd513ef5528dde822d7d
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-17 08:16:56 +0000

    Create data_min_max_fu.R

---
+++ analysis/comparisons/data_min_max_fu.R
@@ -0,0 +1,41 @@
+################################################################################
+
+# This script:
+# - calculates min and max follow-up dates for each subgroup
+
+################################################################################
+
+library(tidyverse)
+
+# read study parameters
+study_parameters <- readr::read_rds(
+  here::here("output", "lib", "study_parameters.rds"))
+
+## read data
+# covariates data
+data_covariates <- readr::read_rds(
+  here::here("output", "data", "data_covariates.rds")) 
+
+# processed data
+data_processed <- readr::read_rds(
+  here::here("output", "data", "data_processed.rds")) 
+
+data_min_max_fu <- data_covariates %>%
+  inner_join(
+    data_processed,
+    by = "patient_id"
+  ) %>%
+  group_by(subgroup) %>%
+  summarise(
+    min_fu_date = min(start_k_date),
+    max_fu_date = max(end_k_date),
+    .groups = "keep"
+  ) %>% 
+  ungroup() %>%
+  mutate(across(max_fu_date,
+                ~ pmin(as.Date(study_parameters$end_date), .x)))
+
+readr::write_rds(
+  data_min_max_fu,
+  here::here("output", "lib", "data_min_max_fu.rds")
+)
