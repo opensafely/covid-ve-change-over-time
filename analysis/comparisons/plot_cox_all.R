6acdaffe278739abf87e4ae524b8cfa2e06f857e
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-02-21 11:55:14 +0000

    Update plot_cox_all.R

--- analysis/comparisons/plot_cox_all.R
+++ analysis/comparisons/plot_cox_all.R
@@ -37,6 +37,12 @@ min_max_fu_dates <- readr::read_rds(
   mutate(across(ends_with("date"),
                 ~ str_c(day(.x), " ", month(.x, label=TRUE))))
 
+# gg plot pallete
+gg_color_hue <- function(n, transparency = 1) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100, alpha = transparency)[1:n]
+}
+
 # scale for x-axis
 K <- study_parameters$max_comparisons
 ends <- seq(2, (K+1)*4, 4)
@@ -75,6 +81,7 @@ model_tidy_tibble <- bind_rows(
 ################################################################################
 
 legend_width <- 15
+xlab <- "Weeks since second dose"
 
 plot_data <- model_tidy_tibble %>%
   filter(
@@ -116,12 +123,13 @@ plot_data <- model_tidy_tibble %>%
                 factor,
                 levels = subgroup_labels[subgroups_order],
                 labels = str_wrap(subgroups[subgroups_order], 25))) %>%
-  mutate(across(k_labelled,
+  mutate(k_labelled_dates = k_labelled) %>%
+  mutate(across(k_labelled_dates,
                 ~ case_when(
                   k == 1
-                  ~ str_c(.x, "\n \nFollow-up\nfrom\n", min_fu_date),
+                  ~ str_c(.x, "\nFrom\n", min_fu_date),
                   k == max_k
-                  ~ str_c(.x, "\n \nLatest\nfollow-up\n", max_fu_date),
+                  ~ str_c(.x, "\nTo\n", max_fu_date),
                   TRUE ~ as.character(.x)))) %>%
   arrange(k) %>%
   group_by(k, subgroup, outcome, comparison) %>%
@@ -129,36 +137,43 @@ plot_data <- model_tidy_tibble %>%
   ungroup() %>%
   mutate(order = order1 + order2)
     
+
 # spacing of points on plot
 position_dodge_val <- 0.6
 # shape of points
 point_shapes <- 21:24
 
+################################################################################
 # vaccine vs unvaccinated
-plot_data %>%
+plot_vax <- plot_data %>%
   filter(comparison != "both") %>%
   ggplot(aes(
-    x = reorder(k_labelled,order), 
+    x = reorder(k_labelled_dates, order), 
     y = estimate, 
     colour = comparison, 
     fill = comparison, 
     shape = subgroup)) +
   geom_hline(aes(yintercept=1), colour='grey') +
-  geom_linerange(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = position_dodge_val)) +
-  geom_point(position = position_dodge(width = position_dodge_val)) +
+  geom_linerange(
+    aes(ymin = conf.low, ymax = conf.high),
+    position = position_dodge(width = position_dodge_val)) +
+  geom_point(
+    position = position_dodge(width = position_dodge_val)
+    ) +
   facet_grid(outcome ~ subgroup, switch = "y", scales = "free", space = "free_x") +
   scale_y_log10(
     name = "Hazard ratio\n<--   favours vaccine   |   favours no vaccine   -->",
-    breaks = c(0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5),
-    limits = c(0.01, 5),
+    breaks = c(0.01, 0.05, 0.2, 0.5, 1, 2),
+    limits = c(0.01, 2),
     oob = scales::oob_keep
   ) +
   labs(
-    x = "weeks since second dose"
+    x = xlab
   ) +
   scale_colour_discrete(name = NULL) +
   scale_fill_discrete(guide = "none") +
-  scale_shape_manual(guide = "none", values = point_shapes) +
+  scale_shape_manual(guide = "none", 
+                     values = point_shapes) +
   theme_bw() +
   theme(
     panel.border = element_blank(),
@@ -168,8 +183,8 @@ plot_data %>%
     
     axis.title.x = element_text(size = 8, margin = margin(t = 10, r = 0, b = 0, l = 0)),
     axis.title.y = element_text(size = 8, margin = margin(t = 0, r = 0, b = 0, l = 0)),
-    axis.text.x = element_text(size=6),
-    axis.text.y = element_text(size=6),
+    axis.text.x = element_text(size=7),
+    axis.text.y = element_text(size=7),
     
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank(),
@@ -188,104 +203,75 @@ plot_data %>%
     legend.position = "bottom",
     legend.text = element_text(size=8)
   ) 
-
-# brand comparison
-plot_data %>%
-  filter(comparison != "both") %>%
-  ggplot(aes(
-    x = reorder(k_labelled,order), 
-    y = estimate, 
-    colour = comparison, 
-    fill = comparison, 
-    shape = subgroup)) +
-  geom_hline(aes(yintercept=1), colour='grey') +
-  geom_linerange(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = position_dodge_val)) +
-  geom_point(position = position_dodge(width = position_dodge_val)) +
-  facet_grid(outcome ~ subgroup, switch = "y", scales = "free", space = "free_x") +
-  scale_y_log10(
-    name = "Hazard ratio\n<--   favours vaccine   |   favours no vaccine   -->",
-    breaks = c(0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5),
-    limits = c(0.01, 5),
-    oob = scales::oob_keep
-  ) +
-  labs(
-    x = "weeks since second dose"
-  ) +
-  scale_colour_discrete(name = NULL) +
-  scale_fill_discrete(guide = "none") +
-  scale_shape_manual(guide = "none", values = point_shapes) +
-  theme_bw() +
-  theme(
-    panel.border = element_blank(),
-    axis.line.y = element_line(colour = "black"),
-    
-    axis.text = element_text(size=8),
-    
-    axis.title.x = element_text(size = 8, margin = margin(t = 10, r = 0, b = 0, l = 0)),
-    axis.title.y = element_text(size = 8, margin = margin(t = 0, r = 0, b = 0, l = 0)),
-    axis.text.x = element_text(size=6),
-    axis.text.y = element_text(size=6),
-    
-    panel.grid.minor.x = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    strip.background = element_blank(),
-    strip.placement = "outside",
-    strip.text.y.left = element_text(angle = 0),
-    strip.text = element_text(size=8),
-    
-    panel.spacing = unit(0.8, "lines"),
-    
-    plot.title = element_text(hjust = 0),
-    plot.title.position = "plot",
-    plot.caption.position = "plot",
-    plot.caption = element_text(hjust = 0, face= "italic"),
-    
-    legend.position = "bottom",
-    legend.text = element_text(size=8)
-  ) 
-
 # save the plot
-ggsave(filename = here::here("output", "models_cox", "images", glue("hr_all.png")),
-       width=20, height=15, units="cm")
+ggsave(plot_vax,
+       filename = here::here("output", "models_cox", "images", glue("hr_vax.png")),
+       width=24, height=15, units="cm")
 
+################################################################################
+# brand comparison
+palette_adj <- gg_color_hue(3, transparency = 1)
+i <- 2 # green
 
-# %>%
-#   mutate(panel_order = case_when(
-#     subgroup %in% subgroup_labels[4] & outcome %in% unname(outcomes[4]) ~ 1,
-#     subgroup %in% subgroup_labels[4] & outcome %in% unname(outcomes[5]) ~ 2,
-#     subgroup %in% subgroup_labels[1] & outcome %in% unname(outcomes[4]) ~ 3,
-#     subgroup %in% subgroup_labels[1] & outcome %in% unname(outcomes[5]) ~ 4,
-#     subgroup %in% subgroup_labels[4] & outcome %in% unname(outcomes[2]) ~ 5,
-#     subgroup %in% subgroup_labels[4] & outcome %in% unname(outcomes[3]) ~ 6,
-#     subgroup %in% subgroup_labels[1] & outcome %in% unname(outcomes[2]) ~ 7,
-#     subgroup %in% subgroup_labels[1] & outcome %in% unname(outcomes[3]) ~ 8,
-#     subgroup %in% subgroup_labels[3] & outcome %in% unname(outcomes[4]) ~ 9,
-#     subgroup %in% subgroup_labels[3] & outcome %in% unname(outcomes[5]) ~ 10,
-#     subgroup %in% subgroup_labels[2] & outcome %in% unname(outcomes[4]) ~ 11,
-#     subgroup %in% subgroup_labels[2] & outcome %in% unname(outcomes[5]) ~ 12,
-#     subgroup %in% subgroup_labels[3] & outcome %in% unname(outcomes[2]) ~ 13,
-#     subgroup %in% subgroup_labels[3] & outcome %in% unname(outcomes[3]) ~ 14,
-#     subgroup %in% subgroup_labels[2] & outcome %in% unname(outcomes[2]) ~ 15,
-#     subgroup %in% subgroup_labels[2] & outcome %in% unname(outcomes[3]) ~ 16,
-#     TRUE ~ NA_real_
-#   )) %>%
-#   mutate(across(subgroup, 
-#                 factor, 
-#                 levels = subgroup_labels[subgroups_order],
-#                 labels = subgroups[subgroups_order])) %>%
-#   mutate(across(outcome, 
-#                 factor, 
-#                 levels = unname(outcomes[outcomes_order]), 
-#                 labels = names(outcomes[outcomes_order]))) %>%
-#   mutate(panel = str_wrap(str_c(subgroup, outcome, sep = ", "),30)) 
-# 
-# panel_labels <- plot_data %>%
-#   distinct(panel, panel_order) %>%
-#   arrange(panel_order) %>%
-#   select(panel) %>% unname() %>% unlist()
-# 
-# plot_data %>%
-#   mutate(across(panel_order, levels = 1:16, labels = panel_labels)) %>%
-#   ggplot(aes(x = label, y = estimate, colour = comparison)) +
-#   geom_point() +
-#   facet_wrap(~panel_order)
+plot_brand <- plot_data %>%
+  filter(comparison == "both") %>%
+  ggplot(aes(
+    x = reorder(k_labelled, order), 
+    y = estimate, 
+    shape = subgroup)) +
+  geom_hline(aes(yintercept=1), colour='grey') +
+  geom_linerange(
+    aes(ymin = conf.low, ymax = conf.high), 
+    position = position_dodge(width = position_dodge_val),
+    color = palette_adj[i],
+    fill = palette_adj[i]
+    ) +
+  geom_point(
+    position = position_dodge(width = position_dodge_val),
+    color = palette_adj[i],
+    fill = palette_adj[i]
+    ) +
+  facet_grid(outcome ~ ., switch = "y", scales = "free", space = "free_x") +
+  scale_y_log10(
+    name = "Hazard ratio\n<--   favours BNT162b2   |   favours ChAdOx   -->",
+    breaks = c(0.5, 0.75, 1, 1.33, 2),
+    limits = c(0.5, 2),
+    oob = scales::oob_keep
+  ) +
+  labs(
+    x = xlab
+  ) +
+  scale_shape_manual(name = "Subgroup", values = point_shapes) +
+  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    axis.line.y = element_line(colour = "black"),
+    
+    axis.text = element_text(size=8),
+    
+    axis.title.x = element_text(size = 8, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+    axis.title.y = element_text(size = 8, margin = margin(t = 0, r = 0, b = 0, l = 0)),
+    axis.text.x = element_text(size=6),
+    axis.text.y = element_text(size=6),
+    
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    strip.background = element_blank(),
+    strip.placement = "outside",
+    strip.text.y.left = element_text(angle = 0),
+    strip.text = element_text(size=8),
+    
+    panel.spacing = unit(0.8, "lines"),
+    
+    plot.title = element_text(hjust = 0),
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.caption = element_text(hjust = 0, face= "italic"),
+    
+    legend.position = "right",
+    legend.title = element_text(size=8),
+    legend.text = element_text(size=8)
+  ) 
+ggsave(plot_brand,
+       filename = here::here("output", "models_cox", "images", glue("hr_brand.png")),
+       width=12, height=10, units="cm")
