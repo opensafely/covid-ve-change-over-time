06052a3a9c46c53ea006d7a5359b07a560885a00
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-03-09 17:28:15 +0000

    Update plot_cox_all.R

--- analysis/post_release/plot_cox_all.R
+++ analysis/post_release/plot_cox_all.R
@@ -31,7 +31,7 @@ subgroup_labels <- seq_along(subgroups)
 subgroups_order <- c(4,1,3,2)
 
 # define comparisons
-comparisons <- c("BNT162b2", "ChAdOx", "both")
+comparisons <- c("BNT162b2", "ChAdOx1", "both")
 
 # min and max follow-up dates per subgroup
 min_max_fu_dates <- readr::read_rds(
@@ -65,13 +65,15 @@ weeks_since_2nd_vax <- str_c(starts[-(K+1)], ends[-1], sep = "-")
 # read estimates data
 estimates_all <- readr::read_csv(
   here::here(release_folder, "estimates_all.csv")) %>%
-  mutate(across(model, ~as.integer(str_remove(.x, "unadjusted"))))
+  mutate(across(model, ~as.integer(str_remove(.x, "unadjusted")))) %>%
+  mutate(across(comparison, ~str_replace(.x, "ChAdOx", "ChAdOx1")))
 
 # read metareg data
 metareg_results_k <- readr::read_rds(
   here::here(release_folder, "metareg_results_k.rds")) %>%
   select(subgroup, comparison, outcome, k, starts_with("line")) %>%
-  mutate(model=2)
+  mutate(model=2) %>%
+  mutate(across(comparison, ~str_replace(.x, "ChAdOx", "ChAdOx1")))
 
 ################################################################################
 
@@ -538,7 +540,7 @@ plot_unadj_adj <- function(plot_comparison) {
     palette_adj <- gg_color_hue(2, transparency = 1)
     i <- case_when(
       plot_comparison %in% "BNT162b2" ~ 1,  # red 
-      plot_comparison %in% "ChAdOx" ~ 2, # blue
+      plot_comparison %in% "ChAdOx1" ~ 2, # blue
       TRUE ~ NA_real_
     )
     
@@ -714,7 +716,7 @@ plot_unadj_adj <- function(plot_comparison) {
   
 }
 
-for (i in c("BNT162b2", "ChAdOx", "both")) {
+for (i in c("BNT162b2", "ChAdOx1", "both")) {
   plot_unadj_adj(i)
 }
 
