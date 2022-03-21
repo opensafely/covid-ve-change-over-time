f99dbe5cf80dc082191df997b0fe6c33f93c47db
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-03-21 17:46:43 +0000

    Update render_tables.R

--- analysis/post_release/render_tables.R
+++ analysis/post_release/render_tables.R
@@ -3,8 +3,3 @@
 rmarkdown::render(
   here::here("analysis","post_release", "table1_process.Rmd"),
   output_file = here::here("release20220221", "table1_process.docx"))
-
-rmarkdown::render(
-  here::here("analysis","post_release", "table_covariate_estimates_bind.Rmd"),
-  output_file = here::here("release20220221", "table_covariate_estimates_bind.docx"))
-
