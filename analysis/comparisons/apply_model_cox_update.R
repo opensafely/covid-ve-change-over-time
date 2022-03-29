2585285d05505835b67d46fb0b745f1504cdcb22
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-03-29 07:50:34 +0100

    update apply_model_cox_update to model comparison periods separately

--- analysis/comparisons/apply_model_cox_update.R
+++ analysis/comparisons/apply_model_cox_update.R
@@ -17,8 +17,8 @@ args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   comparison <- "BNT162b2"
-  subgroup_label <- 1
-  outcome <- "anytest"
+  subgroup_label <- 4
+  outcome <- "covidadmitted"
   
 } else{
   comparison <- args[[1]]
@@ -37,131 +37,148 @@ subgroups <- readr::read_rds(
   here::here("output", "lib", "subgroups.rds"))
 subgroup <- subgroups[subgroup_label]
 
+# read study parameters
+study_parameters <- readr::read_rds(
+  here::here("output", "lib", "study_parameters.rds"))
+K <- study_parameters$max_comparisons
+
 ################################################################################
-# read data
-model_input <-  readr::read_rds(
-  here::here("output", "preflight", "data", glue("model_input_{comparison}_{subgroup_label}_{outcome}.rds"))
-)
-
-# specify filename_suffix for saving models
-filename_suffix <- glue("{comparison}_{subgroup_label}_{outcome}")
-
-if (is.null(model_input)) {
-  # model input null if not enough events, see preflight.R for threshold
-  # save empty tibbles to avoid errors
-  readr::write_rds(
-    tibble(),
-    here::here("output", "models_cox", "data", glue("modelcox_glance_{filename_suffix}.rds"))) 
+for (kk in 1:K) {
   
-  readr::write_rds(
-    tibble(),
-    here::here("output", "models_cox", "data", glue("modelcox_tidy_{filename_suffix}.rds"))) 
-  
-  readr::write_rds(
-    tibble(),
-    here::here("output", "models_cox", "data", glue("modelx_{comparison}_{filename_suffix}.rds")), 
-    compress="gz")
-  
-} else {
-  
-  model_varlist <- readr::read_rds(
-    here::here("output", "lib", "model_varlist.rds")
+  # read data
+  model_input <-  readr::read_rds(
+    here::here("output", "preflight", "data", glue("model_input_{comparison}_{subgroup_label}_{outcome}_{kk}.rds"))
   )
   
-  data_cox <- model_input$data
-  formulas <- model_input$formulas
+  # specify filename_suffix for saving models
+  filename_suffix <- glue("{comparison}_{subgroup_label}_{outcome}_{kk}")
   
-  ################################################################################
-  # define formulas
   
-  model_names = c(
-    "1" = "Unadjusted",
-    "2" = "Adjusting for demographics + clinical"
-  )
-  
-  formula_cox_1 <- formulas$unadjusted
-  formula_cox_2 <- formulas$unadjusted %>% 
-    update(formulas$demographic) %>% 
-    update(formulas$clinical)
-
-  ################################################################################
-  # apply and save models (coxph within functions was causing dramas with broom)
-  
-  # set max iterations
-  opt_control <- coxph.control(iter.max = 30)
-  
-  coxmods <- list()
-  cat(glue("...... fitting unadjusted model ......"), "\n")
-  coxmods[[1]] <- coxph(
-    formula = formula_cox_1,
-    data = data_cox,
-    # robust = TRUE,
-    # id = patient_id,
-    cluster = patient_id,
-    na.action = "na.fail",
-    control = opt_control)
-  
-  readr::write_rds(
-    coxmods[[1]],
-    here::here("output", "models_cox", "data", glue("model1_{filename_suffix}.rds")),
-    compress = "gz"
-  )
-  
-  cat(glue("...... fitting adjusted model ......"), "\n")
-  coxmods[[2]] <- coxph(
-    formula = formula_cox_2,
-    data = data_cox,
-    # robust = TRUE,
-    # id = patient_id,
-    cluster = patient_id,
-    na.action = "na.fail",
-    control = opt_control)
-  
-  readr::write_rds(
-    coxmods[[2]],
-    here::here("output", "models_cox", "data", glue("model2_{filename_suffix}.rds")),
-    compress = "gz"
-  )
-  
-  ################################################################################
-  # process output
-  glance <- list()
-  tidy <- list()
-  for (i in seq_along(coxmods)) {
+  if (is.null(model_input)) {
+    # model input null if not enough events, see preflight.R for threshold
+    # save empty tibbles to avoid errors
+    readr::write_rds(
+      tibble(),
+      here::here("output", "models_cox", "data", glue("modelcox_glance_{filename_suffix}.rds"))) 
     
-    coxmod <- coxmods[[i]]
+    readr::write_rds(
+      tibble(),
+      here::here("output", "models_cox", "data", glue("modelcox_tidy_{filename_suffix}.rds"))) 
     
-    glance[[i]] <-
-      broom::glance(coxmod) %>%
-      add_column(
-        model = i,
-        convergence = coxmod$info[["convergence"]],
-        ram = format(object.size(coxmod), units="GB", standard="SI", digits=3L),
-        .before = 1
-      )
+    readr::write_rds(
+      tibble(),
+      here::here("output", "models_cox", "data", glue("modelx_{comparison}_{filename_suffix}.rds")), 
+      compress="gz")
     
-    tidy[[i]] <-
-      broom.helpers::tidy_plus_plus(
-        coxmod,
-        exponentiate = FALSE
-      ) %>%
-      tidy_add_n(
-        coxmod
-      ) %>%
-      add_column(
-        model = i,
-        .before=1
-      )
+  } else {
     
+    model_varlist <- readr::read_rds(
+      here::here("output", "lib", "model_varlist.rds")
+    )
+    
+    data_cox <- model_input$data
+    formulas <- model_input$formulas
+    
+    ################################################################################
+    # define formulas
+    
+    model_names = c(
+      "1" = "Unadjusted",
+      "2" = "Adjusting for demographics + clinical"
+    )
+    
+    formula_cox_1 <- formulas$unadjusted
+    formula_cox_2 <- formulas$unadjusted %>% 
+      update(formulas$demographic) %>% 
+      update(formulas$clinical)
+    
+    ################################################################################
+    # apply and save models (coxph within functions was causing dramas with broom)
+    
+    # set max iterations
+    opt_control <- coxph.control(iter.max = 30)
+    
+    coxmods <- list()
+    cat(glue("...... fitting unadjusted model ......"), "\n")
+    coxmods[[1]] <- coxph(
+      formula = formula_cox_1,
+      data = data_cox,
+      # robust = TRUE,
+      # id = patient_id,
+      cluster = patient_id,
+      na.action = "na.fail",
+      control = opt_control)
+    
+    readr::write_rds(
+      coxmods[[1]],
+      here::here("output", "models_cox", "data", glue("model1_{filename_suffix}.rds")),
+      compress = "gz"
+    )
+    
+    cat(glue("...... fitting adjusted model ......"), "\n")
+    coxmods[[2]] <- coxph(
+      formula = formula_cox_2,
+      data = data_cox,
+      # robust = TRUE,
+      # id = patient_id,
+      cluster = patient_id,
+      na.action = "na.fail",
+      control = opt_control)
+    
+    readr::write_rds(
+      coxmods[[2]],
+      here::here("output", "models_cox", "data", glue("model2_{filename_suffix}.rds")),
+      compress = "gz"
+    )
+    
+    ################################################################################
+    # process output
+    glance <- list()
+    tidy <- list()
+    for (i in seq_along(coxmods)) {
+      
+      coxmod <- coxmods[[i]]
+      
+      glance[[i]] <-
+        broom::glance(coxmod) %>%
+        add_column(
+          model = i,
+          convergence = coxmod$info[["convergence"]],
+          ram = format(object.size(coxmod), units="GB", standard="SI", digits=3L),
+          .before = 1
+        )
+      
+      tidy[[i]] <-
+        broom.helpers::tidy_plus_plus(
+          coxmod,
+          exponentiate = FALSE
+        ) %>%
+        tidy_add_n(
+          coxmod
+        ) %>%
+        add_column(
+          model = i,
+          .before=1
+        )
+      
+    }
+    
+    model_glance <- bind_rows(glance)
+    readr::write_rds(
+      model_glance,
+      here::here("output", "models_cox", "data", glue("modelcox_glance_{filename_suffix}.rds"))) 
+    
+    model_tidy <- bind_rows(tidy)
+    readr::write_rds(
+      model_tidy,
+      here::here("output", "models_cox", "data", glue("modelcox_tidy_{filename_suffix}.rds"))) 
   }
   
-  model_glance <- bind_rows(glance)
-  readr::write_rds(
-    model_glance,
-    here::here("output", "models_cox", "data", glue("modelcox_glance_{filename_suffix}.rds"))) 
-  
-  model_tidy <- bind_rows(tidy)
-  readr::write_rds(
-    model_tidy,
-    here::here("output", "models_cox", "data", glue("modelcox_tidy_{filename_suffix}.rds"))) 
 }
+
+
+
+
+
+
+
