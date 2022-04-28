3a8a19e4b3b3a4905fd11615c82444ff93ddd67e
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-04-28 17:14:16 +0100

    Create instructions.R

---
+++ analysis/post_release/instructions.R
@@ -0,0 +1,30 @@
+# define release folder
+release_folder <- here::here("output", "release_objects")
+
+
+### Flow chart
+source(here::here("analysis", "post_release", "flow.R")) 
+
+### Table 1
+source(here::here("analysis", "post_release", "table1_process"))
+
+rmarkdown::render(
+  here::here("analysis","post_release", "table1_process.Rmd"),
+  knit_root_dir = release_folder,
+    output_file = here::here(release_folder, "table1_process.docx"))
+
+
+
+### Metaregression
+
+# Run waning_metareg.do in Stata
+
+source(here::here("analysis", "post_release", "data_metareg_process.R"))
+
+source(here::here("analysis", "post_release", "data_metareg_k.R"))
+
+### Results plots
+
+source(here::here("analysis", "post_release", "plot_cox_all.R"))
+
+### Appendix
