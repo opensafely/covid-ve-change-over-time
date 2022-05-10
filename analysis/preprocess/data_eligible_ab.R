c5652ff01c16e0ec5900e99b33adc1d4af944363
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-05-10 09:20:21 +0100

    Update data_eligible_ab.R

--- analysis/preprocess/data_eligible_ab.R
+++ analysis/preprocess/data_eligible_ab.R
@@ -46,6 +46,17 @@ eligibility_count <- eligibility_count %>%
     stage = "a-in"
   )
 
+# remove if died before elig_date + 42 days
+data_eligible_a <- data_eligible_a %>%
+  filter(is.na(death_date) | death_date > elig_date + days(42))
+
+eligibility_count <- eligibility_count %>%
+  add_row(
+    description = "Samples dead before elig_date + 42 days removed.",
+    n =  n_distinct(data_eligible_a$patient_id),
+    stage = "a-ex"
+  )
+
 # remove if aged over 120 (to avoid probable errors)
 data_eligible_a <- data_eligible_a %>%
   filter(age_2 <= 120)
@@ -68,55 +79,6 @@ data_eligible_a <- data_eligible_a %>%
 eligibility_count <- eligibility_count %>%
   add_row(
     description = "Samples with missing ethnicity, sex, imd, region removed.",
-    n =  n_distinct(data_eligible_a$patient_id),
-    stage = "a-ex"
-  )
-
-# remove if evidence of covid infection on or before elig_date + 42 days
-# COVID admission
-data_eligible_a <- data_eligible_a %>%
-  filter(
-    ! (
-      !is.na(covid_any_date) &
-        (covid_any_date <= elig_date + days(42)) &
-        covid_event %in% "covidadmitted"
-    ))
-
-eligibility_count <- eligibility_count %>%
-  add_row(
-    description = "Samples with prior COVID admission removed.",
-    n =  n_distinct(data_eligible_a$patient_id),
-    stage = "a-ex"
-  )
-
-# positive COVID test
-data_eligible_a <- data_eligible_a %>%
-  filter(
-    ! (
-      !is.na(covid_any_date) &
-        (covid_any_date <= elig_date + days(42)) &
-        covid_event %in% "postest"
-    ))
-
-eligibility_count <- eligibility_count %>%
-  add_row(
-    description = "Samples with prior positive COVID test removed.",
-    n =  n_distinct(data_eligible_a$patient_id),
-    stage = "a-ex"
-  )
-
-# probable COVID 
-data_eligible_a <- data_eligible_a %>%
-  filter(
-    ! (
-      !is.na(covid_any_date) &
-        (covid_any_date <= elig_date + days(42)) &
-        covid_event %in% "probable"
-    ))
-
-eligibility_count <- eligibility_count %>%
-  add_row(
-    description = "Samples with prior probable COVID removed.",
     n =  n_distinct(data_eligible_a$patient_id),
     stage = "a-ex"
   )
