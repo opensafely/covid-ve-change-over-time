25e2c4a3b6f7bbd5a02f38adf01d67eac9e9b4c9
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-05-12 11:46:05 +0100

    Update data_input_process.R

--- analysis/preprocess/data_input_process.R
+++ analysis/preprocess/data_input_process.R
@@ -227,25 +227,3 @@ readr::write_rds(
   here::here("output", "data", "data_processed.rds"), 
   compress="gz")
 
-################################################################################
-# check distribution of number of positive tests, probably covid recordings and hospitalisations
-# this will be used to determine how many recurring variables to define in study_definition_covs
-
-# redaction functions
-source(here::here("analysis", "functions", "redaction_functions.R"))
-
-data_processed %>%
-  select(patient_id, ends_with("_n")) %>%
-  pivot_longer(cols = -patient_id) %>%
-  group_by(name, value) %>%
-  count() %>%
-  ungroup() %>%
-  mutate(across(n, ~ceiling_any(.x, to = 7))) %>%
-  ggplot(aes(x = value, y = n)) +
-  geom_bar(stat = "identity", width = 1) +
-  facet_wrap(~name, nrow=3, scales = "free")
-
-ggsave(
-  filename = here::here("output", "eda", "outcomes_n.png"),
-  width = 14, height = 20, units = "cm"
-)
