32c2beb5be9f9d921d737b26682821aa6d382c46
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-05-16 15:11:24 +0100

    Update check_prior_covid.R

--- analysis/preprocess/check_prior_covid.R
+++ analysis/preprocess/check_prior_covid.R
@@ -19,12 +19,27 @@ data_processed %>%
   group_by(name, value) %>%
   count() %>%
   ungroup() %>%
+  arrange(name, desc(value)) %>%
+  group_by(name) %>%
+  mutate(
+    cml_n = cumsum(n),
+    thresh_value = if_else(
+      # choose threshold to keep at least 95% of samples
+      cml_n > 0.05*nrow(data_processed),
+      value,
+      NA_integer_
+      )
+    ) %>%
+  mutate(across(thresh_value, max, na.rm = TRUE))  %>%
+  ungroup %>%
   mutate(across(n, ~log(ceiling_any(.x, to = 7)))) %>%
   filter(value <=20) %>% # to avoid very large numbers messing up the scales.
   ggplot(aes(x = value, y = n)) +
   geom_bar(stat = "identity", width = 1) +
+  geom_vline(aes(xintercept = thresh_value), colour = "magenta", linetype = "dashed") +
   facet_wrap(~name, nrow=5) +
-  labs(x = "n tests (truncated at 20)", y = "log(n) patients")
+  labs(x = "n tests (truncated at 20)", y = "log(n) patients") +
+  theme_bw()
 
 ggsave(
   filename = here::here("output", "eda", "prior_covid_outcomes_n.png"),
